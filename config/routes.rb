6ac0c40745f033640ca47de8da2a013da217dcb6
Ian Knox <knoxid@gmail.com>
2017-05-03 11:54:35 -0600

    ssl cert

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'homepage#show'
 
   get '/chart' => 'homepage#github_chart', as: :github_chart
+  get '/.well-known/acme-challenge/:id' => 'certs#letsencrypt'
 end
